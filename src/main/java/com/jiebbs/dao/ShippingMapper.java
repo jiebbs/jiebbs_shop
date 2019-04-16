76d21ea195cda466765899ef5b0eb55c53abf8ec
jiebbs <jiebbs@126.com>
2019-04-16 18:02:31 +0800

    使用mybaties-generator插件创建dao,pojo和mapper

---
+++ src/main/java/com/jiebbs/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.jiebbs.dao;
+
+import com.jiebbs.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
---
+++ src/main/java/com/jiebbs/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.jiebbs.dao;
+
+import com.jiebbs.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
