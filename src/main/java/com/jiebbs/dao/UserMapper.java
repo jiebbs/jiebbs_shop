7c5a36fffa4ed12c0aee90bc3bf38910d1aac8c5
jiebbs <jiebbs@126.com>
2019-04-17 17:46:09 +0800

    更新了用户模块---完成了登录接口的编写

--- src/main/java/com/jiebbs/dao/UserMapper.java
+++ src/main/java/com/jiebbs/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package com.jiebbs.dao;
 
 import com.jiebbs.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,8 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    int checkUsername(@Param("username") String username);
+
+    User checkLogin(@Param("username") String username,@Param("password") String password);
 }
